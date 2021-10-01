2758ec1ce596ca7a8ec61cac25c9b212e821fb81
Md Arifuzzaman <md.arifuzzaman@citytech.cuny.edu>
2021-10-01 15:59:53 -0400

    ADD: added PWA feature to the web app.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,6 +19,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 
